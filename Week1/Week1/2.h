4b6accf52dd9671c636808c66218a6a2d73d0cba
A-Salty-Fish <1309032417@qq.com>
2020-09-26 21:53:55 +0800

    完成第一周作业

--- Week1/Week1/2.h
+++ Week1/Week1/2.h
@@ -3,7 +3,7 @@
 int SumsBiggerNodes(BTree<int>* head, int k)
 {
 	if (head == nullptr) return 0;
-	return SumsBiggerNodes(head->Left, k) + 
-		SumsBiggerNodes(head->Right, k) + 
-		head->value == k ? 1 : 0;
+	return SumsBiggerNodes(head->Left, k) +
+		SumsBiggerNodes(head->Right, k) +
+		head->value >= k ? 1 : 0;
 }
