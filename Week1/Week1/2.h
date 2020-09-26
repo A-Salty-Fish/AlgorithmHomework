8591ae4d568b8336312ed3e929ae168418ba1c8b
A-Salty-Fish <1309032417@qq.com>
2020-09-26 20:25:28 +0800

    第二题

---
+++ Week1/Week1/2.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "BTree.h"
+int SumsBiggerNodes(BTree<int>* head, int k)
+{
+	if (head == nullptr) return 0;
+	return SumsBiggerNodes(head->Left, k) + 
+		SumsBiggerNodes(head->Right, k) + 
+		head->value == k ? 1 : 0;
+}
